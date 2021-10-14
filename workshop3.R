f773d1db2ab267f9a7d082728ea1dd3d3f53fb88
Chi <chihim.edmond@gmail.com>
2021-10-14 17:00:43 +1100

    14 Oct 2021

--- workshop3.R
+++ workshop3.R
@@ -6,6 +6,7 @@
 
 # Either ISO-8601 date or year/week works!
 
+library(tidyverse)
 tuesdata <- tidytuesdayR::tt_load('2021-05-18')
 tuesdata <- tidytuesdayR::tt_load(2021, week = 21)
 
@@ -36,4 +37,90 @@ survey_AUD_female<-survey%>%
   )
 
 survey%>%select(timestamp,gender,currency)
-    
+
+
+survey_USD<-
+  filter(
+    annual_salary >= 50000  
+  )
+
+
+min(survey_USD$annual_salary)
+
+min(survey_USD$timestamp)
+max(survey_USD$timestamp)
+
+
+survey_USD<-survey_USD%>%
+  filter(
+    annual_salary > 0
+  )
+
+
+
+#min(survey_USD%>%arrange(annual_salary))
+
+survey_USD%>%arrange(
+  desc(annual_salary)
+)
+
+
+#What are industries in the dataset?
+survey_USD%>%distinct(industry)
+
+# how many under each industry
+survey_USD%>%count(industry,gender,sort=TRUE)
+
+#What is the gender distribution?
+survey_USD%>%count(gender,industry,sort=TRUE)
+
+
+#How many responses from each age catergory and gender?
+survey_USD%>%count(how_old_are_you,gender,sort=TRUE)
+
+
+#Group_by
+survey_USD_grouped<-survey_USD%>%
+  group_by(gender,industry)%>%
+  mutate(
+    avr_salary=mean(annual_salary)
+  )
+
+
+#Mutate
+survey_USD_avr<-survey_USD%>%
+  mutate(
+    avr_salary=mean(annual_salary)
+  )
+
+#transmute
+survey_total<-
+  survey_USD%>%
+  group_by(industry)%>%
+  transmute(
+    total_salary=annual_salary+as.numeric(other_monetary_comp),
+    min_slary=min(annual_salary),
+    max_salary=min(annual_salary),
+    total_salary_what=total_salary
+)
+
+#summarise
+survey_USD_summars<-survey_USD%>%
+  group_by(gender)%>%
+  summarise(
+    avr_salary=mean(annual_salary),
+    min_salary=min(annual_salary),
+    max_salary=min(annual_salary)
+  )%>%
+    ungroup()
+
+#parse_number
+survey_USD%>%
+  parse_number(how_old_are_you)
+
+class(survey_USD$how_old_are_you)
+
+#case_when
+
+
+
